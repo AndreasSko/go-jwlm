445efa5f614007231e141ba2c083e54ec19b5158
Andreas Skorczyk <me@andreas-sk.de>
2020-10-30 13:46:58 +0100

    CMD: Compare two backups
    Using
    `go-jwlm compare <left-backup> <right-backup>`
    it is possible to compare two backup files to see
    if their contents are equal, even if the order of entries
    might be different.

---
+++ cmd/compare_test.go
@@ -0,0 +1,66 @@
+// +build !windows
+
+package cmd
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/AlecAivazis/survey/v2/terminal"
+	expect "github.com/Netflix/go-expect"
+	"github.com/tj/assert"
+)
+
+func Test_compare(t *testing.T) {
+	t.Parallel()
+
+	tmp, err := ioutil.TempDir("", "go-jwlm")
+	assert.NoError(t, err)
+	defer os.RemoveAll(tmp)
+
+	emptyFilename := filepath.Join(tmp, "empty.jwlibrary")
+	leftFilename := filepath.Join(tmp, "left.jwlibrary")
+	rightFilename := filepath.Join(tmp, "right.jwlibrary")
+	assert.NoError(t, emptyDB.ExportJWLBackup(emptyFilename))
+	assert.NoError(t, leftDB.ExportJWLBackup(leftFilename))
+	assert.NoError(t, rightDB.ExportJWLBackup(rightFilename))
+
+	RunCmdTest(t,
+		func(t *testing.T, c *expect.Console) {
+			_, err := c.ExpectString("Backups are NOT equal")
+			assert.NoError(t, err)
+			_, err = c.ExpectEOF()
+			assert.NoError(t, err)
+		},
+		func(t *testing.T, c *expect.Console) {
+			compare(leftFilename, emptyFilename, terminal.Stdio{In: c.Tty(), Out: c.Tty(), Err: c.Tty()})
+			time.Sleep(time.Millisecond * 150) // So it does not finish before go-expect finished
+		})
+
+	RunCmdTest(t,
+		func(t *testing.T, c *expect.Console) {
+			_, err := c.ExpectString("✅ Backups are equal")
+			assert.NoError(t, err)
+			_, err = c.ExpectEOF()
+			assert.NoError(t, err)
+		},
+		func(t *testing.T, c *expect.Console) {
+			compare(leftFilename, leftFilename, terminal.Stdio{In: c.Tty(), Out: c.Tty(), Err: c.Tty()})
+			time.Sleep(time.Millisecond * 150) // So it does not finish before go-expect finished
+		})
+
+	RunCmdTest(t,
+		func(t *testing.T, c *expect.Console) {
+			_, err := c.ExpectString("Backups are equal")
+			assert.NoError(t, err)
+			_, err = c.ExpectEOF()
+			assert.NoError(t, err)
+		},
+		func(t *testing.T, c *expect.Console) {
+			compare(rightFilename, rightFilename, terminal.Stdio{In: c.Tty(), Out: c.Tty(), Err: c.Tty()})
+			time.Sleep(time.Millisecond * 150) // So it does not finish before go-expect finished
+		})
+}
